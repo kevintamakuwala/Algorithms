f8f4fa4b2701115100d365212a94520298fe0591
kevintamakuwala <kevintamakuwala16@gmail.com>
2023-05-12 17:58:59 +0530

    Create min-cost-climbing-stairs.cpp

---
+++ Dp/min-cost-climbing-stairs.cpp
@@ -0,0 +1,22 @@
+/*
+    Problem Link: https://leetcode.com/problems/min-cost-climbing-stairs/
+
+    You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+    You can either start from the step with index 0, or the step with index 1.
+    Return the minimum cost to reach the top of the floor.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+vector<int>dp(1001,-1);
+class Solution {    
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n=cost.size();
+        for(int i=2;i<n;i++)
+        {
+            cost[i]+=min(cost[i-1],cost[i-2]);
+        }
+        return min(cost[n-1],cost[n-2]);
+    }
+};
